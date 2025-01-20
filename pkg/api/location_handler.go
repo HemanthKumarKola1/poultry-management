fef6c58a18ac1acef2ae4d6e466eff18bc672328
HemanthKumarKola1 <hemanthkumarkola1@gmail.com>
2025-01-20 19:51:47 +0530

    updated two routes

--- pkg/api/location_handler.go
+++ pkg/api/location_handler.go
@@ -2,21 +2,23 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	db "poultry-management.com/internal/db/sqlc"
 	"poultry-management.com/pkg/repo"
 )
 
 type LocationHandler struct {
-	loc    repo.LocationRepo
-	Router *gin.Engine
-	config Config
+	locrepo repo.LocationRepo
+	Router  *gin.Engine
+	config  Config
 }
 
 func NewLocationHandler(l repo.LocationRepo, r *gin.Engine) *LocationHandler {
 	lh := &LocationHandler{
-		loc:    l,
-		Router: r,
+		locrepo: l,
+		Router:  r,
 	}
 	setupLocationRoutes(lh)
 	return lh
@@ -25,7 +27,7 @@ func NewLocationHandler(l repo.LocationRepo, r *gin.Engine) *LocationHandler {
 func setupLocationRoutes(lh *LocationHandler) {
 
 	protected := lh.Router.Group("/tenant-location")
-	protected.Use(authMiddleware([]byte(lh.config.JWTSecret)))
+	protected.Use(authLocationMiddelware([]byte(lh.config.JWTSecret)))
 	{
 		protected.POST("/create", lh.createLocation)
 		protected.GET("/get", lh.getLocations)
@@ -33,8 +35,30 @@ func setupLocationRoutes(lh *LocationHandler) {
 }
 
 func (h *LocationHandler) getLocations(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Locations fetched successfully"})
+	locIDStr := c.GetHeader("location")
+	intVal, err := strconv.Atoi(locIDStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	location, err := h.locrepo.GetLocation(c.Request.Context(), int32(intVal), int32(c.GetInt("tenant_id")))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch locations"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"locations": location})
 }
+
 func (h *LocationHandler) createLocation(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Locations created successfully"})
+	var req db.CreateLocationParams
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	loc, err := h.locrepo.CreateNewTenantLocation(c.Request.Context(), req, int32(c.GetInt("tenant_id")))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Locations created successfully", "location": loc})
 }
--- pkg/api/middelware.go
+++ pkg/api/middelware.go
@@ -28,3 +28,25 @@ func authMiddleware(jwtKey []byte) gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func authLocationMiddelware(jwtKey []byte) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("AuthHandlerorization")
+		if tokenString == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "AuthHandlerorization header is missing"})
+			return
+		}
+
+		claims, err := auth.ValidateJWT(tokenString, jwtKey)
+		if err != nil || !(claims.Role == "admin" || claims.Role == "master" || claims.Role == "super_admin") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token/user"})
+			return
+		}
+
+		c.Set("user_id", claims.UserID)
+		c.Set("tenant_id", claims.TenantID)
+		c.Set("username", claims.Username)
+		c.Set("role", claims.Role)
+		c.Next()
+	}
+}
